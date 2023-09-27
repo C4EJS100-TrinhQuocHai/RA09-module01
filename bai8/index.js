df1f20da55847d3d2cc9a9c2df16c14a0aa8f0d5
trinhquochai <trinhquochai12@gmail.com>
2023-09-27 13:32:01 +0700

    add

---
+++ bai8/index.js
@@ -0,0 +1,21 @@
+// var fullName;
+//var a;
+// fullName = "nguyễn văn an";
+// console.log(fullName);
+// var a=7;
+// khai báo biến a có giá trị là 7.
+// a=9;// gán lại giá trị
+// var a=15; // khai báo lại
+
+//  firstName;
+// console.log(a);
+// let a=5;
+
+// khai báo biến a có giá trị là 5
+// {
+//     var a=8;
+// }
+// console.log(a);
+let fullname="nguyễn văn an";
+console.log("222");
+
